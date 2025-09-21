3e1f8c6a6261f23f1dfd182d7827f97897aee66d
Emmanuel Ayo <avinnewealth@gmail.com>
2025-09-21 23:30:18 +0100

    Change Variable name

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
-opr = input("Choose the operation (+, -, *, /):").strip()
+operation = input("Choose the operation (+, -, *, /):").strip()
 
 
 match opr:
