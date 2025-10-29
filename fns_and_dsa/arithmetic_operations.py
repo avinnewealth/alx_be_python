a4b5e64f9ad3588657936fd248573d12565fc5b1
Emmanuel Ayo <avinnewealth@gmail.com>
2025-10-29 21:15:21 +0100

    Updated 2

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,7 +11,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Cannot divide by zero"
-            else:
+            elif num2 > 0:
                return num1 / num2
-        case _ :
-            return "Sorry! You can only Add, Subtract, Multiply, and Divide. Try again"
+            else:
+                return "Sorry! You can only Add, Subtract, Multiply, and Divide. Try again"
