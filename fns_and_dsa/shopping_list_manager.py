cb438cf3251110fe46b8825d12045477a0c322a5
Emmanuel Ayo <avinnewealth@gmail.com>
2025-11-01 21:18:45 +0100

    Add Script for managing shopping

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,28 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            shopping_list.append(input("Please Add an Item: "))
+            pass
+        elif choice == '2':
+            shopping_list.remove(input("What item do you want to remove?: "))
+            pass
+        elif choice == '3':
+            print(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+           
