c24fd27e148db2d85d529f1d2c299d7d911934e4
Emmanuel Ayo <avinnewealth@gmail.com>
2025-11-04 03:14:03 +0100

    add script for unit test simple calc example

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,31 @@
+import unittest
+from simple_calculator import SimpleCalculator as SM
+
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SM()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 1), 3)
+        self.assertEqual(self.calc.add(-2,3),1)
+        self.assertEqual(self.calc.add(-5, -7), -12)
+
+    def test_substract(self):
+        self.assertEqual(self.calc.subtract(2, 1), 1)
+        self.assertEqual(self.calc.subtract(-2,3),-5)
+        self.assertEqual(self.calc.subtract(-5, -2), -3)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 1), 2)
+        self.assertEqual(self.calc.multiply(-2,3),-6)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(2, 1), 2)
+        self.assertEqual(self.calc.divide(-6,3),-2)
+        self.assertEqual(self.calc.divide(0, 5), 0)
+
+        self.assertIsNone(self.calc.divide(5, 0))
